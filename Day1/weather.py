0633aa654f5221ded6eb724cd3b43c35f4efe249
Ujjan <u_msft@outlook.com>
2025-05-14 05:04:51 +1000

    Folder

---
+++ Day1/weather.py
@@ -0,0 +1,9 @@
+temperature = float(input("Enter the temperature in Celsius: "))
+
+if temperature > 30:
+    print("It's hot outside. Stay hydrated!")
+    print("Consider wearing light clothing.")
+    print("Avoid going out during peak sun hours.")
+
+else:
+    print("The weather is pleasant.")
