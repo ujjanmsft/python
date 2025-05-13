e064dd503f88da2dc0f2c22cd0e25f1e27a36872
Ujjan <u_msft@outlook.com>
2025-05-14 04:46:09 +1000

    vote

---
+++ vote.py
@@ -0,0 +1,10 @@
+age = int(input("Enter your age: "))
+
+if age >= 18:
+    print("You are eligible to vote.")
+    print("Please make sure you are registered to vote.")
+    print("Please do vote on election day.")
+else:
+    print("You are not eligible to vote.")
+    print("Please wait until you turn 18 to register and vote.")
+    print("In the meantime, you can learn about the voting process.")
