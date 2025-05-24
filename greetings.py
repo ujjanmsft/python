21d44f8265d62e16e41e3ba7bbac4ab432f77adb
Ujjan <u_msft@outlook.com>
2025-05-25 06:29:05 +1000

    Dash app

--- greetings.py
+++
@@ -1,8 +0,0 @@
-# Enter your name
-name = input("What is your name? ")
-
-# Enter your age
-age = input("How old are you? ")
-
-# Greet user with name and age
-print("Hello " + name + "! " + "Your age is "+ age + ".")
